fcd8d33d5992e14c3674048bd3c598faec827e7f
Mike Ladderbush <mikeladderbush@gmail.com>
2024-06-05 18:46:53 -0400

    Module 5 completed baseline

--- travlr/app.js
+++ travlr/app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 var handlebars = require('hbs');
+
+// Bring in the database
+require('./app_api/models/db');
 
 var app = express();
 
@@ -25,17 +31,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Wire routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- travlr/app_api/models/travlr.js
+++ travlr/app_api/models/travlr.js
@@ -8,7 +8,7 @@ const tripSchema = new mongoose.Schema({
     start: { type: Date, required: true },
     resort: { type: String, required: true },
     perPerson: { type: String, required: true },
-    image: { type: String, required: true},
+    image: { type: String, required: true },
     description: { type: String, required: true }
 });
 const Trip = mongoose.model('trips', tripSchema);
